c059f8593e6b37f0c576970ee814134ccf0b9bc7
Streltsov Valerii <st.valera04@gmail.com>
2023-01-31 23:04:59 +0200

    done fifth exersise

---
+++ src/js/task-05.js
@@ -0,0 +1,17 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+function addName(event) {
+    outputEl.textContent = event.currentTarget.value;
+}
+
+function defaultName() {
+    if(outputEl.textContent === '' || outputEl.textContent === ' '){
+        outputEl.textContent = 'Anonymous';
+    }
+    
+
+}
+
+inputEl.addEventListener('input', addName);
+inputEl.addEventListener('blur', defaultName)
