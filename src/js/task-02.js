61f38254250a1ceb642e0f2cd04b8a79494a14c3
Streltsov Valerii <st.valera04@gmail.com>
2023-01-31 21:02:22 +0200

    Done second exersise

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients')
+
+
+
+const arrayOfLi = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  
+  li.textContent = ingredient;
+  li.classList.add('item');
+  
+  return li;
+});
+
+
+ulEl.append(...arrayOfLi);
