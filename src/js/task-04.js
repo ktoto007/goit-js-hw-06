7bc215c021447e7f2030a1f84bfa48d3f184e6e0
Streltsov Valerii <st.valera04@gmail.com>
2023-01-31 22:24:21 +0200

    Done fourth exersise

---
+++ src/js/task-04.js
@@ -0,0 +1,19 @@
+const btnDecrement = document.querySelector('button[data-action = "decrement"]');
+const btnIncrement = document.querySelector('button[data-action = "increment"]');
+const valueEl = document.querySelector('#value');
+let counterValue = 0;
+
+function onValueIncrement() {
+    counterValue = counterValue + 1;
+    valueEl.textContent = counterValue;
+};
+
+function onValueDicrement() {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+
+
+btnDecrement.addEventListener('click', onValueDicrement);
+btnIncrement.addEventListener('click', onValueIncrement);
+
