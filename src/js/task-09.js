4579266c2eec2e001e7ee8088698a6f10f778b09
Streltsov Valerii <st.valera04@gmail.com>
2023-02-01 09:04:20 +0200

    Done nineth exersise

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const btnEl = document.querySelector('.change-color');
+const textEL = document.querySelector('.color')
+const bodyEL = document.querySelector('.body');
+
+console.log(bodyEL);
+function getNewBackgroundColor() {
+  const newColor = getRandomHexColor();
+  bodyEL.style.background = newColor;
+  textEL.textContent = newColor;
 }
+
+btnEl.addEventListener('click', getNewBackgroundColor)
