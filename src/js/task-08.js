3bb13e7890fd615656163cc7d23fc35f06123701
Streltsov Valerii <st.valera04@gmail.com>
2023-02-01 08:40:30 +0200

    Done eighth exersise

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+function onFormProcessing(event){
+    event.preventDefault();
+
+    const {
+        elements:  { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("There are empty fields in the form!");
+      };
+
+    console.log(`Email: ${email.value}`);
+    console.log(`Password: ${password.value}`);
+}
+
+formEl.addEventListener('submit', onFormProcessing)
